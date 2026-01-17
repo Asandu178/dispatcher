eae02fa09dbea1131b25eca97a8c1d39c66e3cb2
Asandu178 <dragos310131@gmail.com>
2026-01-17 16:50:42 +0200

    km

--- local-procedure-call/src/dispatcher.c
+++ local-procedure-call/src/dispatcher.c
@@ -18,6 +18,7 @@
 #define DETACHED 1
 
 int static epollfd;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 typedef struct {
     char Version[20];
@@ -44,16 +45,65 @@ void *execute(void *args) {
     return NULL;
 }
 
-void handle_install(char *buf) {
+void *handle_install(void *ctx) {
+    pthread_mutex_lock(&lock);
+    int installfd = *(int *)ctx;
+    char *buf = calloc(200, 1);
+    int size = read(installfd, buf, 200);
     
     uint16_t numar = *(uint16_t*)buf;
     numar = be16toh(numar);
-    char* text = (char*)(buf + 2);
+    char *text = calloc(numar, 1);
+    strcpy(text, (buf + 2));
+
+    if (makePipe(text, 0777) < 0) {
+        printf("Failed to make pipe\n");
+    }
+
+    int fd = open(text, O_RDONLY | O_CREAT, 0644);
+
+    char installBuf[0xff] = {0};
+
+    int x = read(fd, installBuf, sizeof(installBuf));
+    printf("%d\n", x);
+
+    uint8_t m_VersionLen = *(uint8_t *)installBuf;
+    u_int16_t m_CpnLen = be16toh(*(uint16_t *)(installBuf + 1));
+    u_int16_t m_RpnLen = be16toh(*(uint16_t *)(installBuf + 3));
+    u_int16_t m_ApLen = be16toh(*(uint16_t *)(installBuf + 5));
+    char *content = installBuf + 7;
+        printf("%s\n", content);
+
+    char *version = calloc(m_VersionLen + 1, 1);
+    char *call_pipe = calloc(m_CpnLen + 1, 1);
+    char *return_pipe = calloc(m_RpnLen + 1, 1);
+    char *access_path = calloc(m_ApLen + 1, 1);
+    
+    strncpy(version, content, m_VersionLen);
+    strncpy(call_pipe, content + m_VersionLen + m_VersionLen, m_CpnLen);
+    strncpy(return_pipe, content + m_CpnLen + m_VersionLen, m_RpnLen);
+    strncpy(access_path, content + m_CpnLen + m_RpnLen + m_VersionLen, m_ApLen);
+
+    printf("%d | %d | %d | %d\n", m_VersionLen, m_CpnLen, m_RpnLen, m_ApLen);
+    printf("%s | %s | %s | %s\n", version, call_pipe, return_pipe, access_path);
 
     printf("%d\n", numar);
     printf("%s\n", text);
     fflush(stdout);
+    close(fd);
+    pthread_mutex_unlock(&lock);
     
+}
+
+void *handle_connection(void *ctx) {
+    pthread_mutex_lock(&lock);
+    int connectionfd = *(int *)ctx;
+
+    char *buf = calloc(200, 1);
+    int size = read(connectionfd, buf, 200);
+    printf("test\n");
+    fflush(stdout);
+    pthread_mutex_unlock(&lock);
 }
 
 int main(void)
@@ -64,11 +114,11 @@ int main(void)
 	pthread_attr_setdetachstate(&attr, DETACHED);
 
 
-    if (mkdir(DISPATCHER_C, 0755) < 0 && access(DISPATCHER_C, F_OK) < 0) {
+    if (mkdir(DISPATCHER_C, 0777) < 0 && access(DISPATCHER_C, F_OK) < 0) {
         perror("mkdir dispatcher");
     }
     
-    if (mkdir(PIPES_C, 0755) < 0 && access(PIPES_C, F_OK) < 0) {
+    if (mkdir(PIPES_C, 0777) < 0 && access(PIPES_C, F_OK) < 0) {
         perror("mkdir pipes");
     }
 
@@ -99,21 +149,19 @@ int main(void)
 
 	while(1) {
 		struct epoll_event res;
+        pthread_t test;
 		w_epoll_wait_infinite(epollfd, &res);
 
 		if (res.events & EPOLLIN) {
 			if (res.data.fd == installfd) {
-                char *buf = calloc(200, 1);
-                int size = read(installfd, buf, 200);
-                handle_install(buf);
-                //write(STDOUT_FILENO, buf, size);
+                pthread_create(&test, &attr, handle_install, &installfd);
             }
 			if (res.data.fd == connectionfd) {
-                char *buf = calloc(200, 1);
-                int size = read(installfd, buf, 200);
-                write(STDOUT_FILENO, buf, size);
+                pthread_create(&test, &attr, handle_connection, &connectionfd);
             }
-		}
+		} else {
+
+        }
 	}
     return 0;
 }
